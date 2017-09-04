a7718bf4d153a2f3fe9fa57515a5c1d3d5fa73bd
jrrodenbusch <jrrodenbusch@gmail.com>
2017-09-04 13:50:58 -0500

    Create empty untitled spreadsheet

--- WindowsFormsApplication1/Startup.cs
+++ WindowsFormsApplication1/Startup.cs
@@ -1,17 +1,29 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Data = Google.Apis.Sheets.v4.Data;
 
 namespace WindowsFormsApplication1
 {
     public partial class Startup : Form
     {
+
+        static string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static string ApplicationName = "BmesMembership";
+
         public Startup()
         {
             InitializeComponent();
@@ -22,7 +34,40 @@ namespace WindowsFormsApplication1
             String date = DateTime.UtcNow.Date.ToString("dd/MM/yyy");
             if(name != "")
             {
-                //TODO: SEND TO SPREADSHEET
+                // Google Sheets
+                UserCredential credential;
+
+                using (var stream = new FileStream("client_secret_new.json", FileMode.Open, FileAccess.Read))
+                {
+                    string credPath = System.Environment.GetFolderPath(
+                        System.Environment.SpecialFolder.Personal);
+                    credPath = Path.Combine(credPath, ".credentials/sheets.googleapis.com-dotnet-quickstart.json");
+
+                    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.Load(stream).Secrets,
+                        Scopes,
+                        "user",
+                        CancellationToken.None,
+                        new FileDataStore(credPath, true)).Result;
+                    Console.WriteLine("Credential file saved to: " + credPath);
+                }
+
+                var service = new SheetsService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = ApplicationName,
+                });
+
+                // TODO: save student data to sheet
+
+                Data.Spreadsheet requestBody = new Data.Spreadsheet();
+                SpreadsheetsResource.CreateRequest request = service.Spreadsheets.Create(requestBody);
+                Data.Spreadsheet response = request.Execute();
+                var spreadsheetID = response.SpreadsheetId;
+
+                Console.WriteLine(response);
+                Console.WriteLine(spreadsheetID);
+
                 this.Visible = false;
                 UTIDLogin signInForm = new UTIDLogin(name, date);
                 signInForm.Show();
